323ea25821c5e4e6526645df8352fab51e8ae852
naim <Vous@exemple.com>
2018-04-22 21:04:15 +0200

    thread expiration 1er version

--- ServerMl/server_manager.ml
+++ ServerMl/server_manager.ml
@@ -1,10 +1,18 @@
 open Server;;
 open Connexion_manager;;
 
+
+let expiration () = Thread.delay 10.0;
+		print_endline "fin de temps repartie";
+		Thread.exit ();;
+
 class server_maj port n =
-   object(s)
+   object(self)
    inherit server port n
 
+	 method  wait_expiration  () = Thread.create (fun x -> expiration ())()
+		
    method treat s sa =
-   ignore( (new connexion_maj s sa true clients tirage dictionnaire)#start())
+   ignore( (new connexion_maj s sa true clients tirage dictionnaire)#start());
+	 ignore (self#wait_expiration ())
    end;;
